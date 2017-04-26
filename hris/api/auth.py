b0c2886f3239c478f8a6b9ce789211abde3ca1b1
Robus Gauli <user@Users-MacBook-Air.local>
2017-04-26 19:33:34 +0545

    Asd

--- hris/api/auth.py
+++ hris/api/auth.py
@@ -189,20 +189,27 @@ def create_update_permission(key):
             except Exception as e:
                 return unauthorized_envelop()
             else:
-                role_id = decoded['role_id']
+                #role_id = decoded['role_id']
                 user_name = decoded['user_name']
-
-                if role_id not in current_app.config:
-                    roles = db_session.query(Role).all()
-                    roles = [role.to_dict() for role in roles]
-                    for role in roles:
-                        current_app.config[role['id']]= role
-            
-                role = current_app.config[role_id][key]
-                if role == 'W' or role =='E':
-                    return func(*args, **kwargs)
+                try:
+                    user = db_session.query(User).filter(User.user_name == user_name).one()
+                except NoResultFound as e:
+                    return record_notfound_envelop()
+                except Exception as e:
+                    return fatal_error_envelop()
                 else:
-                    return unauthorized_envelop()
+                    role_id = user.role_id
+                    if role_id not in current_app.config:
+                        roles = db_session.query(Role).all()
+                        roles = [role.to_dict() for role in roles]
+                        for role in roles:
+                            current_app.config[role['id']]= role
+                    print(current_app.config[role_id])
+                    role = current_app.config[role_id][key]
+                    if role == 'W' or role =='E':
+                        return func(*args, **kwargs)
+                    else:
+                        return unauthorized_envelop()
             
         return wrapper
 
